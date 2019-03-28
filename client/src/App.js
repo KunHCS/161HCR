a2a099aa1aa9819e6faeed75c51f8c2061f0c343
Bryan Mira <bryan.mira@yahoo.com>
2019-03-27 17:34:29 -0700

    save function

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,35 @@ import './App.css';
 import SignatureCanvas from 'react-signature-canvas';
 
 class App extends Component {
+	state = {trimmedDataURL: null}
+  sigPad = {}
+  clear = () => {
+    this._signatureCanvas.clear()
+  }
+  trim = () => {
+    this.setState({trimmedDataURL: this._signatureCanvas.getTrimmedCanvas()
+      .toDataURL('image/png')})
+  }
+
   render() {
+  	let {trimmedDataURL} = this.state
     return (
       <div>
-      <h1>Handwriting Recognition</h1>
+      <h1>Handwriting Recognitionsss</h1>
         <SignatureCanvas penColor='black'
-    canvasProps={{width: 800, height: 800, className: 'sigCanvas'}} />
+    canvasProps={{width: 800, height: 500, className: 'sigCanvas'}} ref={(r) => { this._signatureCanvas = r;}} />
+      <div>
+        <button  onClick={this.clear}>
+          Clear
+        </button>
+        <button  onClick={this.trim}>
+          Save
+        </button>
+      </div>
+      {trimmedDataURL
+        ? <img 
+          src={trimmedDataURL} />
+        : null}
       </div>
     );
   }
